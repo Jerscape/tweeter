970b406b114794c969638bcbf44b5a4efaffc68a
Jerscape <jeremydutton5@gmail.com>
2023-08-07 16:10:27 -0500

    tweet form styled

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("document ready")
+  const tweetForm = document.querySelector('tweet-text')
+  tweetForm.addEventListener('submit')
+});
