b834ced376fc340e569d30062e08844d5db799d5
Jerscape <jeremydutton5@gmail.com>
2023-08-08 14:55:05 -0500

    ammended to use this keyword

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,49 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("document ready")
-  const tweetForm = document.querySelector('tweet-text')
-  tweetForm.addEventListener('submit')
+  // let count = 0
+  // //const tweetForm = document.querySelector('tweet-text')
+  // const tweetForm2 = document.getElementById('tweet-text')
+  // tweetForm2.addEventListener('input', (event) => {
+
+  //   count = count + 1
+  //   const start = 140
+  //   const remaining = start - count
+  //   //console.log(event.target.value)
+  //   console.log(remaining)
+  //   //update inner text of output 
+
+  //   if(remaining >= 0){
+  //     document.getElementById("output").innerText = remaining
+
+  //   } else {
+  //     document.getElementById("output").style.color="red"
+  //     document.getElementById("output").innerText = remaining
+      
+  //   }
+    
+   
+  //   //consider accounting for backspace/delete
+  //   //consider making it red if it is below 0
+     
+  // })
+
+  
+  $('#tweet-text').on('input', function(event) {
+    console.log(this)//for testing purposes
+    console.log(event.target.value);
+    //const count = event.target.value.trim().length;
+    const count = $(this).val().length;
+    const remaining = 140-count
+    $("#output").text(remaining)
+
+    if(remaining >= 0){
+      $("#output").css("color", "black")
+    } else {
+      $("#output").css("color", "red")
+    }
+
+    $(".classname")
+
+  });
+
 });
